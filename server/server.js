e7efd4852ed9e4298100957b32cae2f45df2e81b
vuongc <christopher.k.vuong@gmail.com>
2016-02-17 12:17:29 +0100

    Set default port on MySql server, cannot access express routes with sequelize right now

--- server/server.js
+++ server/server.js
@@ -4,23 +4,22 @@ var models = require('./app/models');
 var http = require('http');
 
 // Set port in environment variable
-app.set('port', process.env.PORT || 3300);
+app.set('port', process.env.PORT || 3306);
 
 // Synchronize models with database and start server
 models.sequelize.sync().then(function() {
-  // app.listen(3300, function() {
-  //   console.log('App is listening on port 3300');
-  // });
 
   // Create server
-  var server = http.createServer(app);
+  var server = http.createServer(app, function() {
+    console.log('Create server console log');
+  });
 
   // Start server
-  server.listen(3300, function() {
-    debug('App [http] is listening on port 3300');
+  server.listen(3306, function() {
+    console.log('App [http] is listening on port 3306');
   });
 
   app.get('/', function(req, res) {
-    debug('Get routes');
+    console.log('Get routes console log');
   });
 });
