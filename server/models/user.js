4a610dc074cdd931af0a32ffde9ea7cdf560ef00
vuongc <christopher.k.vuong@gmail.com>
2016-02-26 19:01:50 +0100

    login and signup are working with passport

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,9 @@
 var database = require('../database/database');
+var bcrypt   = require('bcrypt-nodejs');
 
-var User = database.sequelize.define('user', {
+var User = database.sequelize.define('User', {
   username: { type: database.Sequelize.STRING, unique: true, allowNull: false },
-  password: { type: database.Sequelize.STRING, allowNull: false },
+  password: { type: database.Sequelize.STRING, allowNull: false }
 });
 
 var syncUser = function () {
@@ -13,17 +14,28 @@ var syncUser = function () {
   });
 };
 
+// Password hashing
+var generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+var validPassword = function(password, userPassword) {
+  return bcrypt.compareSync(password, userPassword);
+};
+
+// Database interaction
 var createUser = function(username, password, promise) {
-  User.create({ username: username, password: password }).then(promise);
+  User.create({ username: username, password: generateHash(password) }).then(promise);
 };
 
 var findById = function(id, promise) {
-  User.findById(id).then(promise);
+  User.findById(id, {raw: true}).then(promise);
 };
 
 var findOne = function(username, password, promise) {
   User.findOne({
-    where: { username: username, password: password }
+    raw: true,
+    where: { username: username }
   }).then(promise);
 };
 
@@ -32,10 +44,18 @@ var findAll = function(promise) {
   User.findAll({ raw: true }).then(promise);
 };
 
+// Has to change this, it's ugly
+var build = function(user) {
+  return User.build(user);
+};
+
 module.exports = {
+  build: build,
   syncUser: syncUser,
   createUser: createUser,
   findById: findById,
   findOne: findOne,
-  findAll: findAll
+  findAll: findAll,
+  validPassword: validPassword,
+  generateHash: generateHash
 };
--- server/server.js
+++ server/server.js
@@ -1,25 +1,46 @@
 // Get module
-var express = require('express');
-var http = require('http');
-var bodyParser = require('body-parser');
+var express  = require('express');
+var app      = express();
+var http     = require('http');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
-var app = express();
+var ejs          = require('ejs');
+var session      = require('express-session');
 
-var port = 3306;
+var port = process.env.PORT || 3306;
 
-var users = require('./routes/users');
+// var users = require('./routes/users');
+
+require('./config/passport')(passport); // pass passport for configuration
 
 // Use middlewares
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // Read cookie (needed for auth)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-// Load routes here
-app.use('/users', users);
+// Set the folder where the pages are kept
+app.set('views', __dirname + '/public/views');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+// This avoids having to provide the
+// extension to res.render()
+app.set('view engine', 'html');
+
+// required for passport
+app.use(session({ secret: 'secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// Use middlewares
+app.use('/', express.static(__dirname + '/public'));
+
+// routes ======================================================================
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Open server
 var server = http.createServer(app);
